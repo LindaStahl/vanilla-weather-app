0959ce081722053fba744c69796f1fdc56dbe80b
LindaStahl <Linda_stahl@hotmail.com>
2025-05-13 10:20:13 +0200

    Updated js file
    Fixed search function error

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-function refreshWeather(response) {
+function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
-  let temperature = response.data.temperature.current;
+  let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
   let descriptionElement = document.querySelector("#description-weather");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind");
-  let timeElement = document.querySelector("#current-date");
+  let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#current-temperature-icon");
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
+  temperatureElement.innerHTML = temperature;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-  temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
   let days = [
     "Sunday",
     "Monday",
@@ -30,29 +39,25 @@ function formatDate(date) {
     "Friday",
     "Saturday"
   ];
-  let day = days[date.getDay()];
 
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  return `${day} ${hours}:${minutes}`;
+  let formattedDay = days[date.getDay()];
+  return `${formattedDay} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
   let apiKey = "b2a5adcct04b33178913oc335f405433";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  axios.get(apiUrl).then(refreshWeather);
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
 }
 
-function handleSearchSubmit(event) {
+function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-
   searchCity(searchInput.value);
 }
 
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSearch);
 
 searchCity("Madrid");
